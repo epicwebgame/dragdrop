2bdb47426a5bd3acb139390cc2f9d08293d7d8d2
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2019-04-21 13:10:41 +0530

    Made the same refactoring changes to the Google People API Implicit Flow OAuth 2.0 code that I had made to the Google+ API application. These changes are:
    1. Wrote the code to mitigate against an XSRF attack in the Google People API implementation for the Implicit Flow (OAuth 2).
    
    2. Genericized the getAccessTokenFromUrl function into one that gets the value of a parameter in the query string of the URL. Re-used that same
    function to get the access token as well as the XSRF state from the URL.
    
    3. Yanked out the URL helper function that gets the value of a query string parameter into a separate module named UrlHelper and used it from there and tested the refactored code.

--- JavaScript/OAuth2.v2/Implicit/Google/People/index.js
+++ JavaScript/OAuth2.v2/Implicit/Google/People/index.js
@@ -1,12 +1,13 @@
 import googleOAuth2Config from "./google.config.js";
+import * as urlHelper from "./urlHelper.js";
 
-$(document).ready(function() {
+$(document).ready(async function() {
 
     try {
         var url = window.location.href;
         
         if (/access_token=/.test(url)) {
-            showWelcomeMessage(url, googleOAuth2Config);
+            await showWelcomeMessage(url, googleOAuth2Config);
         } else {
             showSignInLink();
         }
@@ -22,19 +23,38 @@ function showSignInLink() {
     .show();
 }
 
-function showError(message) {
+function showError(error) {
     $("#signInLink").hide();
     $("#welcomeMessage").hide();
 
-    let s = message || "Something went wrong and that's all I know!";
+    let message = "Something went wrong and that's all I know!";
 
+    if (typeof error === "string" && error.trim() !== "") {
+        message = error;
+    } else if (typeof error === "object") {
+        if (error.constructor.name.indexOf("Error") >= 0) {
+            message = error.message;
+        } else {
+            message = error.toString();
+        }
+    }
+    
     let errorMessage = $("#errorMessage");
     errorMessage
-    .html(s)
+    .html(message)
     .show();
 }
 
 async function showWelcomeMessage(url, googleOAuth2Config) {
+
+    if (!xsrfStateValid(url, googleOAuth2Config)) {
+        console.log("The XSRF state parameter did not match. The request may have been tampered with.");
+        throw new Error(`Your communication with the server may not be secure. \
+        Are you browsing the Internet from a public place? \
+        Like an airport or a cafe or a library or a hotel room or a friend's house? \
+        It is recommended that you stop browsing this website from this place.`);
+    }
+
     let accessToken = getAccessTokenFromUrl(url);
     console.log(`accessToken: ${accessToken}`);
     googleOAuth2Config.setAccessToken(accessToken);
@@ -51,26 +71,33 @@ async function showWelcomeMessage(url, googleOAuth2Config) {
 }
 
 function getAccessTokenFromUrl(url) {
-    let keyFragment = "access_token=";
-    let startIndexOfKey;
-    startIndexOfKey = url.indexOf(keyFragment);
+    let accessToken = urlHelper.getValueOfQueryStringParameter(url, "access_token");
     
-    if (startIndexOfKey === -1) {
+    if (!accessToken || accessToken === "") {
         console.log("No access token returned by the Google authorization server.");
-        throw "Sorry, something went wrong and it's not your fault. Please try again in a few hours.";
+        throw new Error("Sorry, something went wrong and it's not your fault. Please try again in a few hours.");
     }
-
-    let startIndex = startIndexOfKey + keyFragment.length;
-    let endIndex = url.length - 1;
-
-    for(let i = startIndex; i < url.length; i++) {
-        if (url[i] === "&") {
-            endIndex = i - 1;
-            break;
-        }
-    }
-
-    let accessToken = url.substr(startIndex, (endIndex - startIndex) + 1);
 
     return accessToken;
+}
+
+function xsrfStateValid(url, googleOAuth2Config) {
+
+    if (!googleOAuth2Config.state || googleOAuth2Config.state === "") {
+        return true;
+    }
+
+    let state = getStateFromUrl(url);
+    
+    if (!state || state === "") {
+        return false;
+    }
+
+    return state === googleOAuth2Config.state;
+}
+
+function getStateFromUrl(url) {
+    let state = urlHelper.getValueOfQueryStringParameter(url, "state");
+
+    return state;
 }
