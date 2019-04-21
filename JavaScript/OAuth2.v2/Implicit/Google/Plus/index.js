3cd79cee19f7b2fb10dd83fd16fdbf66a5c5d82a
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2019-04-21 13:00:37 +0530

    Testing the XSRF attack mitigation code in the Google+ API OAuth 2 Implicit Flow.

--- JavaScript/OAuth2.v2/Implicit/Google/Plus/index.js
+++ JavaScript/OAuth2.v2/Implicit/Google/Plus/index.js
@@ -48,7 +48,10 @@ async function showWelcomeMessage(url, googleOAuth2Config) {
 
     if (!xsrfStateValid(url, googleOAuth2Config)) {
         console.log("The XSRF state parameter did not match. The request may have been tampered with.");
-        throw new Error("Your communication with the server may not be secure. Are you browsing the Internet from a public place? Like an airport or a cafe or a library or a friend's house? It is recommended that you stop browsing this website from this place.");
+        throw new Error(`Your communication with the server may not be secure. \
+        Are you browsing the Internet from a public place? \
+        Like an airport or a cafe or a library or a hotel room or a friend's house? \
+        It is recommended that you stop browsing this website from this place.`);
     }
 
     let accessToken = getAccessTokenFromUrl(url);
@@ -78,6 +81,9 @@ function getAccessTokenFromUrl(url) {
 }
 
 function xsrfStateValid(url, googleOAuth2Config) {
+
+    return false;
+
     if (!googleOAuth2Config.state || googleOAuth2Config.state === "") {
         return true;
     }
