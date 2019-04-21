ebaf8d1ece3554a7949b9559cb5e5837fa7e3739
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2019-04-21 15:52:38 +0530

    WIP: Authorization Code Flow for Google+ API.

---
+++ JavaScript/OAuth2.v2/AuthorizationCode/Google/Plus/Plus/Code/GooglePlusOAuth2Config.cs
@@ -0,0 +1,10 @@
+﻿namespace Plus.Code
+{
+    public class GooglePlusOAuth2Config : OAuth2Config
+    {
+        public override string AuthorizationServerBaseUrl 
+            => "https://accounts.google.com/o/oauth2/v2/auth";
+        public override string ResourceServerBaseUrl 
+            => "https://www.googleapis.com/plus/v1/people/me?";
+    }
+}
--- JavaScript/OAuth2.v2/AuthorizationCode/Google/Plus/Plus/Code/OAuth2Config.cs
+++ JavaScript/OAuth2.v2/AuthorizationCode/Google/Plus/Plus/Code/OAuth2Config.cs
@@ -1,17 +1,50 @@
-﻿namespace Plus.Code
+﻿using System.Text;
+
+namespace Plus.Code
 {
-    public class OAuth2Config
+    public abstract class OAuth2Config
     {
-        public string AuthorizationCodeRedirectBaseUri { get; set; }
-        public string AccessTokenRedirectBaseUri { get; set; }
-        public string ClientId { get; set; }
+        public abstract string AuthorizationServerBaseUrl { get; }
+        public string AuthorizationCodeRedirectBaseUri { get; protected set; }
+        public string ClientId { get; protected set; }
+        public string Scope { get; protected set; }
+        public virtual string ResponseType => "code";
+        public string State { get; protected set; }
+
+
         public string ClientSecret { get; set; }
-        public string State { get; set; }
-        public string Scope { get; set; }
-        public string AuthorizationServerUrl { get; set; }
-        public string ResourceServerUrl { get; set; }
-        public string ResponseType { get; set; }
-        public string Fields { get; set; }
+        public string AccessTokenRedirectBaseUri { get; }
         public string GrantType { get; set; }
+        public string AccessToken { get; set; }
+        public long AccessTokenExpiresInSeconds { get; set; }
+        public string AccessTokenType { get; set; }
+        public string RefreshToken { get; set; }
+        public string Error { get; set; }
+
+
+        public abstract string ResourceServerBaseUrl { get; }
+        public string Fields { get; set; }
+
+        public virtual string GetSignInUrl(
+            string clientId, 
+            string authorizationCodeRedirectBaseUri, 
+            string scope, 
+            string state)
+        {
+            this.ClientId = clientId;
+            this.AuthorizationCodeRedirectBaseUri = authorizationCodeRedirectBaseUri;
+            this.Scope = scope;
+            this.State = state;
+
+            var builder = new StringBuilder(this.AuthorizationServerBaseUrl);
+
+            builder.AppendFormat($"client_id={ClientId}&");
+            builder.AppendFormat($"redirect_uri={AuthorizationCodeRedirectBaseUri}&");
+            builder.AppendFormat($"response_type={ResponseType}&");
+            builder.AppendFormat($"scope={Scope}&");
+            builder.AppendFormat($"state={State}");
+
+            return builder.ToString();
+        }
     }
 }
