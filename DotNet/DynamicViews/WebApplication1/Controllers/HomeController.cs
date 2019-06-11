8201642e55b7fec34e3b842b2f6139ac456823ee
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2019-06-11 19:35:17 +0530

    I wanted to try out if it was possible to add views dynamically into an ASP.NET MVC website without having to re-compile or re-build and re-publish the website to the remote host, simply by X-Copying the newly dynamic view files to the remote host. It appears that I can. Now, the only question that remains is: how do I X-Copy deploy to the Kudu server on Azure?

---
+++ DotNet/DynamicViews/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using WebApplication1.Models;
+using System.Linq;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Articles()
+        {
+            IEnumerable<Article> articles = null;
+
+            using (var context = new PracticeEntities())
+            {
+                articles = context.Articles.ToList();
+            }
+
+            return View(articles);
+        }
+
+        [Route("Articles/{id}")]
+        public ActionResult DisplayArticle(string id)
+        {
+            var viewPath = string.Format($"~/Views/DynamicViews/{id}.cshtml");
+
+            return View((object)viewPath);
+        }
+    }
+}
