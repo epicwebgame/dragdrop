7a8140c7ea22531236b25ca9a9ad23c557f91076
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2019-07-28 19:59:00 +0530

    Tried adding each dragged and dropped file to the form's formData but it is learnt now that one cannot, for security reasons I surmise, add to an existing form's formData on the fly in a POST back scenario. The formData collection is only for AJAX requests.

--- JavaScript/browser/FileUpload/9/Scripts/DragAndDrop.js
+++ JavaScript/browser/FileUpload/9/Scripts/DragAndDrop.js
@@ -22,12 +22,17 @@ function previewFiles(files, event) {
 
         $previewContainer.children().remove();
 
+        let form = document.getElementsByTagName("form")[0];
+        let formData = form.formData || new FormData(form);
+
         for (let file of imageFiles) {
             let src = (window.URL || window.webkitURL).createObjectURL(file);
             let img = `<img src = "${src}" class = "previewImage" />`;
             let newDiv = `<div class = "previewItem"><div class = "previewImageContainer">${img}</div><div class = "previewImageFileName">${file.name}</div></div>`;
 
             $previewContainer.append(newDiv);
+
+            formData.append("files", file, file.name);
         }
 
         $("span").hide();
