81ad5458774e7830821e16936813e4e0f2aad2c8
Sathyaish Chakravarthy <Sathyaish@gmail.com>
2020-01-03 22:20:55 +0530

    I wanted to find out if an expired cookie is sent to the server or not.
    So, for e.g. there was a cookie named AUTH and it has now expired. Will the cookie remain in the Storage section in the Developer Tools window? Will it be sent by the client to the server with the expiration date set to the past date or will it just not be included in the HTTP payload? If I were writing the server side code, should I check for the absence of the cookie like this:
    
    Request.Cookies["AUTH"] == null
    
    or
    
    Request.Cookies["AUTH"].Expires <= DateTime.Now
    
    Also, if a cookie that has been deleted is sent to the server or not. If you're thinking, "Wait! A deleted cookie? Sent to the server? How can something that's been deleted be sent to the server? Won't it have to be re-created in order to be used if it was already deleted?"
    
    No, no! You see, we know that when either the client or the server needs to delete a cookie
    
    Already used cookies a thousand times. There are some things you remember and some things you'll forget. And I need to use them once again, so I want to test out a few things before I write the actual code. I am presently writing the Login functionality for my website https://sathyaish.net.

---
+++ WebConcepts/ClientSendExpiredCookies/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace ClientSendExpiredCookies.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ResetAuthCookieIfExists();
+            return View();
+        }
+        public JsonResult Whatever()
+        {
+            ResetAuthCookieIfExists();
+            return Json(new object(), JsonRequestBehavior.AllowGet);
+        }
+        public JsonResult DeleteAuthCookie()
+        {
+            DeleteAuthCookieIfExists();
+            return Json(new object(), JsonRequestBehavior.AllowGet);
+        }
+        private void DeleteAuthCookieIfExists()
+        {
+            Response.Cookies["AUTH"].Expires = DateTime.Now.AddSeconds(-1);
+        }
+        private void ResetAuthCookieIfExists()
+        {
+            int interval = Request.Cookies["AUTH"] == null ? 60 : 50;
+
+            var expiry = DateTime.UtcNow.AddSeconds(interval);
+
+            var epochTime = expiry.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+
+            Response.Cookies.Set(new System.Web.HttpCookie("AUTH")
+            {   
+                Value = epochTime.ToString(),
+                Expires = expiry
+            });
+        }
+    }
+}
---
+++ WebConcepts/ClientSendExpiredCookies/Scripts/index.js
@@ -0,0 +1,118 @@
+﻿document.addEventListener("DOMContentLoaded", setup);
+
+function setup() {
+    let gmtNowElement = document.getElementById("gmtNow");
+    gmtNowElement.innerHTML = (new Date()).toUTCString();
+
+    window.setInterval(updateCurrentTimeAndAuthCookieExpiryStatus, 1000);
+
+    document.getElementById("makeARequestToServer").addEventListener("click", makeARequestToTheServerClickEventHandler, false);
+    document.getElementById("tellServerToDeleteCookie").addEventListener("click", tellServerToDeleteCookieClickEventHandler, false);
+}
+
+function makeARequestToTheServerClickEventHandler() {
+    let cookieValueElement = document.getElementById("cookieValue");
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/Home/Whatever", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = function (event) {
+        displayAuthCookieValue(cookieValueElement);
+    };
+    xhr.send();
+}
+
+function tellServerToDeleteCookieClickEventHandler() {
+    let cookieValueElement = document.getElementById("cookieValue");
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/Home/DeleteAuthCookie", true);
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.onload = function (event) {
+        displayAuthCookieValue(cookieValueElement);
+    };
+    xhr.send();
+}
+
+function updateCurrentTimeAndAuthCookieExpiryStatus() {
+    displayCurrentTimeInGMT();
+    displayAuthCookieValue();
+}
+
+function displayCurrentTimeInGMT() {
+    let gmtNowElement = document.getElementById("gmtNow");
+    if (gmtNowElement) {
+        let now = new Date();
+        gmtNowElement.innerHTML = now.toUTCString();
+    }
+}
+
+function displayAuthCookieValue() {
+    let authCookieValue = getCookieValue("AUTH");
+    let cookieValueElement = document.getElementById("cookieValue");
+    let expiresElement = document.getElementById("expires");
+
+    if (authCookieValue) {
+        let expiryEpochTime = authCookieValue * 1000;
+        let expiryDateObject = new Date(expiryEpochTime);
+        cookieValueElement.innerText = expiryDateObject.toUTCString();
+
+        if (expiryEpochTime <= Date.now()) {
+            cookieValueElement.addClass("expired").removeClass("alive");
+            expiresElement.style.display = "none";
+        } else {
+            cookieValueElement.addClass("alive").removeClass("expired");
+            expiresElement.style.display = "";
+        }
+    } else {
+        expiresElement.style.display = "none";
+        cookieValueElement.removeClass("alive").addClass("expired");
+        cookieValueElement.innerText = "No such cookie exists";
+    }
+}
+
+function getCookie(name) {
+    let cookiesString = document.cookie;
+
+    if (!cookiesString) return;
+
+    let allCookies = cookiesString.split(";");
+    let numCookies = allCookies.length;
+    if (numCookies > 0) {
+        for (let i = 0; i < numCookies; i++) {
+            let cookie = allCookies[i];
+            while (cookie.charAt(0) === ' ') {
+                cookie = cookie.substring(1);
+            }
+
+            if (cookie.indexOf(name) === 0) {
+                return cookie;
+            }
+        }
+    }
+}
+
+function getCookieValue(key) {
+    let cookie = getCookie(key);
+    if (cookie) {
+        return cookie.substring(key.length + 1, cookie.length);
+    }
+}
+
+HTMLElement.prototype.hasClass = function hasClass(className) {
+    return (` ${this.className} `.replace(/[\n\t\r]/g, " ").indexOf(className) > -1);
+};
+
+HTMLElement.prototype.addClass = function addClass(className) {
+    if (!this.hasClass(className)) {
+        this.className += ` ${className} `;
+    }
+
+    return this;
+};
+
+HTMLElement.prototype.removeClass = function removeClass(className) {
+    if (this.classList.contains(className)) {
+        this.classList.remove(className);
+    }
+
+    return this;
+};
